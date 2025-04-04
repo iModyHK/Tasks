82d516b87149afd670515dc1488d4795add3618f
iModyHK <48744133+iModyHK@users.noreply.github.com>
2025-04-04 12:04:00 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-
 const cors = require('cors');
 const dotenv = require('dotenv');
+
 const authRoutes = require('./routes/auth');
 const timeLogRoutes = require('./routes/timelog');
 const settingsRoutes = require('./routes/settings');
@@ -18,11 +17,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/timelog', timeLogRoutes);
 app.use('/api/settings', settingsRoutes);
-
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
